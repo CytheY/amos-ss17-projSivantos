218b872de9d82f54b61e18d77c81453aef1f3083
Julian <Schnei.Julian@gmail.com>
2017-05-23 07:54:21 -0700

    now throws exception correctly

--- raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackendTests/CommonFiles/Networking/ClientServerTests.cs
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackendTests/CommonFiles/Networking/ClientServerTests.cs
@@ -57,6 +57,7 @@ namespace RaspberryBackendTests
         public void TestNullRequest()
         {
             tcpClient.sendObject(null);
+            connection.receiveObject();
         }
     }
 }
